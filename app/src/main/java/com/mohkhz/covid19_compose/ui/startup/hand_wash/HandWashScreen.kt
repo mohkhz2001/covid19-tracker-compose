f4158c0779df2f2bdac2bc4ce0e89989e81fac72
Mohammadmehdi Khajehzadeh <mohammad.mehdi.khz@gmail.com>
2022-03-21 00:14:22 +0330

    location runtime permission removed -> use the ip to find the location

--- app/src/main/java/com/mohkhz/covid19_compose/ui/startup/hand_wash/HandWashScreen.kt
+++ app/src/main/java/com/mohkhz/covid19_compose/ui/startup/hand_wash/HandWashScreen.kt
@@ -1,19 +1,11 @@
 package com.mohkhz.covid19_compose.ui.startup.hand_wash
 
-import android.Manifest
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.Dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.mohkhz.covid19_compose.R
-import com.mohkhz.covid19_compose.isPermanentlyDenied
 import com.mohkhz.covid19_compose.ui.startup.component.StartUpComponent
 import com.mohkhz.covid19_compose.util.UiEvent
 import kotlinx.coroutines.flow.collect
@@ -27,53 +19,12 @@ fun HandWashScreen(
     viewModel: HandWashViewModel = hiltViewModel()
 ) {
 
-    val permissionsState = rememberMultiplePermissionsState(
-        permissions = listOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-        )
-    )
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(
-        key1 = lifecycleOwner,
-        effect = {
-            val observer = LifecycleEventObserver { _, event ->
-                if (event == Lifecycle.Event.ON_START) {
-                    permissionsState.launchMultiplePermissionRequest()
-                }
-            }
-            lifecycleOwner.lifecycle.addObserver(observer)
-
-            onDispose {
-                lifecycleOwner.lifecycle.removeObserver(observer)
-            }
-        }
-    )
 
     LaunchedEffect(key1 = true) {
         viewModel.uiEvent.collect {
             when (it) {
                 is UiEvent.Navigate -> {
-
-                    permissionsState.permissions.forEach { perm ->
-                        when (perm.permission) {
-                            Manifest.permission.ACCESS_FINE_LOCATION -> {
-                                when {
-                                    perm.hasPermission -> {
-                                        onNavigate(it)
-                                    }
-                                    perm.shouldShowRationale -> {
-                                        onNavigate(it)
-                                    }
-                                    perm.isPermanentlyDenied() -> {
-                                        Log.d("test fun ", "b")
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-//                    onNavigate(it)
+                    onNavigate(it)
                 }
                 else -> Unit
             }
